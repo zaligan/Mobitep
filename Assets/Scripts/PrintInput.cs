0c126bb91f7aa87d01a8f6838fbfa19d99aad8d7
KaitoIto <kaito.itou08@gmail.com>
2024-03-15 12:31:04 +0900

    Unityプロジェクトに変更

--- Assets/Scripts/PrintInput.cs
+++ Assets/Scripts/PrintInput.cs
@@ -6,27 +6,19 @@ public class PrintInput : MonoBehaviour
 {
     private MouseInputReader m_mouseInputReader;
 
-    /// <summary>
-    /// EditorWindowで右に設定したデバイスの入力値
-    /// </summary>
     private Vector2 m_rightInputValueVec;
 
-    /// <summary>
-    /// EditorWindowで左に設定したデバイスの入力値
-    /// </summary>
     private Vector2 m_leftInputValueVec;
 
     // Start is called before the first frame update
     void Start()
     {
-        // 同じGameObjectにアタッチされているMouseInputReaderを取得
         m_mouseInputReader = GetComponent<MouseInputReader>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // 要素数が 0 より大きい場合のみ入力値を取得
         if (0 < m_mouseInputReader.GetRightInputValueListCount())
         {
             m_rightInputValueVec = m_mouseInputReader.GetRightInputValueList();
